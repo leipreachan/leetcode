119fe74ad34ed2c8ecc726680b028c7e78eb6942
Artem <2929348+leipreachan@users.noreply.github.com>
2021-01-31 13:07:10 +0000

    squares-of-a-sorted-array

---
+++ squares-of-a-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+* @param {number[]} nums
+* @return {number[]}
+*/
+var sortedSquares = function(nums) {
+let temp = [];
+for(let i =0; i<nums.length; i++) {
+let abs=Math.abs(nums[i]);
+if (temp[abs] === undefined) {
+temp[abs] = 1;
+} else {
+temp[abs]++;
+}
+}
+let result=[];
+for (let i = 0; i<temp.length;i++) {
+if (temp[i] !== undefined) {
+for(let j=0; j<temp[i];j++) {
+result.push(i*i);
+}
+}
+}
+return result;
+};
