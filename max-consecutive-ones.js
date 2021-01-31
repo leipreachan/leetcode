f45e4d6e42ae921469e6f94dae8b9921077533c8
Artem <2929348+leipreachan@users.noreply.github.com>
2021-01-31 13:09:08 +0000

    code format

--- max-consecutive-ones.js
+++ max-consecutive-ones.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-  let max=0, cur=0;
-  for(let i=0; i<nums.length;i++) {
-if(nums[i]===1){
-    cur++;
-}
-    max = max > cur ? max : cur;
-      if (nums[i]===0){
-    cur=0;
-}
-    
-  } 
+var findMaxConsecutiveOnes = function (nums) {
+    let max = 0, cur = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            cur++;
+        }
+        max = max > cur ? max : cur;
+        if (nums[i] === 0) {
+            cur = 0;
+        }
+
+    }
     return max;
 };
--- numbers-with-even-digits-number.js
+++ numbers-with-even-digits-number.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-const hasEvenNumberDigits = function(number) {
-  let result = 0;
-  if ((number > 9 &&
-      number <= 99) || (number > 999 && number <= 9999) || number === 100000) {
-    result = 1;
-  }
-  return result;
+const hasEvenNumberDigits = function (number) {
+    let result = 0;
+    if ((number > 9 &&
+        number <= 99) || (number > 999 && number <= 9999) || number === 100000) {
+        result = 1;
+    }
+    return result;
 }
-var findNumbers = function(nums) {
-  let result = 0;
-  for (let i = 0; i < nums.length; i++) {
-    result += hasEvenNumberDigits(nums[i]);
-  }
-  return result;
+var findNumbers = function (nums) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        result += hasEvenNumberDigits(nums[i]);
+    }
+    return result;
 };
